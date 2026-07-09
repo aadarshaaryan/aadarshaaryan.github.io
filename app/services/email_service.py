a27ab90acc9ea4d99028c3c19eafb7c65f09815d
aadarshkumar-dev <dev.aadarshkumar@gmail.com>
2026-07-09 22:03:28 +0530

    ldsj

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,27 +1,43 @@
-from flask_mail import Message
-from app import mail
-from threading import Thread
-from flask import current_app
+# from flask_mail import Message
+# from app import mail
+# from threading import Thread
+# from flask import current_app
 
-def send_async_email(app, msg):
-    # Flask-Mail requires an active application context to access configuration fields safely
-    with app.app_context():
-        mail.send(msg)
+# def send_async_email(app, msg):
+#     # Flask-Mail requires an active application context to access configuration fields safely
+#     with app.app_context():
+#         mail.send(msg)
+
+# def send_email(to, subject, body):
+#     """
+#     Send an HTML formatted email asynchronously using background threading.
+#     """
+#     # Grab a reference to the active production application instance
+#     app = current_app._get_current_object()
+    
+#     msg = Message(
+#         subject=subject,
+#         recipients=[to],
+#         html=body
+#     )
+    
+#     # Spin up an isolated background thread to handle the network traffic socket handshake
+#     thr = Thread(target=send_async_email, args=[app, msg])
+#     thr.start()
+#     return thr
+
+import os
+import resend
+
+resend.api_key = os.getenv("RESEND_API_KEY")
+
 
 def send_email(to, subject, body):
-    """
-    Send an HTML formatted email asynchronously using background threading.
-    """
-    # Grab a reference to the active production application instance
-    app = current_app._get_current_object()
-    
-    msg = Message(
-        subject=subject,
-        recipients=[to],
-        html=body
-    )
-    
-    # Spin up an isolated background thread to handle the network traffic socket handshake
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
+    params = {
+        "from": "Afflux <onboarding@resend.dev>",
+        "to": [to],
+        "subject": subject,
+        "html": body,
+    }
+
+    return resend.Emails.send(params)
